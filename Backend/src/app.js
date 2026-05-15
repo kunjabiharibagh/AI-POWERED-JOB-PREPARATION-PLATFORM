14376886590443c45f78ce46281438c7e9de5093
kunjabiharibagh <kbbagh2005@github.com>
2026-05-15 18:55:22 +0530

    cors update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,14 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:process.env.CLIENT_URL|| "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin:
+      process.env.CLIENT_URL ||
+      "https://ai-powered-job-preparation-platform.vercel.app",
+    credentials: true,
+  })
+);
 
 
 /* require all the routes here */
