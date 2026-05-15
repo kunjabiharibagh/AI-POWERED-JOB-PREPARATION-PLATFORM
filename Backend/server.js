000b8b4c2f0026bd949bd7cce0ba58a0fa795649
kunjabiharibagh <kbbagh2005@github.com>
2026-05-15 19:10:06 +0530

    cors update

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
 
-connectToDB()
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+connectToDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
